4dbf063d24fbd124310ce9d38e9018e698db076e
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-09-20 18:14:51 +0200

    Setup

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Kacper | Porfolio Website",
+  description: "ToDO",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} 
+      bg-lime-50 text-lime-950 relative`}>
+        <div className="bg-[#dbeafe] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#bfdbfe] absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        
+        {children}
+        </body>
+    </html>
+  );
+}
