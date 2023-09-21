ba8ac6ffa40c0374e24d3e66b89bb2c1cc84b057
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-09-21 23:02:57 +0200

    Add Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} 
-      bg-lime-50 text-lime-950 relative`}>
+      bg-lime-50 text-lime-950 relative h-[5000px]`}>
         <div className="bg-[#dbeafe] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#bfdbfe] absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         
+        <Header />
         {children}
         </body>
     </html>
