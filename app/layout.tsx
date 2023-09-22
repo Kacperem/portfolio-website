9dcb950465680a075feb33d071b9c4df1eee4e2d
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-09-22 19:31:46 +0200

    Add Hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,14 +16,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} 
-      bg-lime-50 text-lime-950 relative h-[5000px]`}>
+      <body
+        className={`${inter.className} 
+      bg-lime-50 text-lime-950 relative h-[5000px] pt-28 sm:pt-36`}
+      >
         <div className="bg-[#dbeafe] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#bfdbfe] absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        
+
         <Header />
+
         {children}
-        </body>
+      </body>
     </html>
   );
 }
