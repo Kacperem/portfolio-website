98c119b937a9d8b97dda92382d31aa88da84e01a
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-09-23 12:18:49 +0200

    Add Divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Divider from "@/components/divider";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Hero />
+      <Divider />
     </main>
   );
 }
