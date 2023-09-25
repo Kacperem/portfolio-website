539c115111a96a1359abb1a7363e03359ae214d0
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-09-25 22:45:50 +0200

    Add Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +19,14 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${inter.className} 
-      bg-lime-50 text-lime-950 relative h-[5000px] pt-28 sm:pt-36`}
+      bg-lime-50 text-lime-950 relative pt-28 sm:pt-36`}
       >
         <div className="bg-[#dbeafe] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#bfdbfe] absolute top-[-1rem] -z-10 left-[11rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
         <Header />
-
         {children}
+        <Footer />
       </body>
     </html>
   );
