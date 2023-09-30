c801e5a39e95cae79a3918c507c9a6f70299d64b
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-09-30 15:42:29 +0200

    Add Experience

--- lib/data.ts
+++ lib/data.ts
@@ -1,56 +1,88 @@
+import React from "react";
+import { LuGraduationCap } from "react-icons/lu";
+import { CgWorkAlt } from "react-icons/cg";
+
 export const links = [
-    {
-      name: "Home",
-      hash: "#home",
-    },
-    {
-      name: "About",
-      hash: "#about",
-    },
-    {
-      name: "Projects",
-      hash: "#projects",
-    },
-    {
-      name: "Skills",
-      hash: "#skills",
-    },
-    {
-      name: "Experience",
-      hash: "#experience",
-    },
-    {
-      name: "Contact",
-      hash: "#contact",
-    },
-  ] as const;
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
 
-  export const projectsData = [
-    {
-      title: "Memory Palace",
-      description:
+export const projectsData = [
+  {
+    title: "Memory Palace",
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu.",
-      tags: ["React", "Material UI", "Tailwind", "Formik", "Framer Motion"],
-      imageUrl: "https://images.unsplash.com/photo-1617472592135-72a181d04b27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGl0JTIwYXBwfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
-    },
-    {
-      title: "Fit App",
-      description:
+    tags: ["React", "Material UI", "Tailwind", "Formik", "Framer Motion"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1617472592135-72a181d04b27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGl0JTIwYXBwfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
+  },
+  {
+    title: "Fit App",
+    description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu. soon",
-      tags: ["React", "Tailwind", "Redux"],
-      imageUrl: "https://images.unsplash.com/photo-1595675024853-0f3ec9098ac7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFwcCUyMG1lbW9yeSUyMHBhbGFjZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
-    }
-  ] as const;
+    tags: ["React", "Tailwind", "Redux"],
+    imageUrl:
+      "https://images.unsplash.com/photo-1595675024853-0f3ec9098ac7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFwcCUyMG1lbW9yeSUyMHBhbGFjZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
+  },
+] as const;
 
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Tailwind",
+  "Framer Motion",
+  "Git",
+] as const;
 
-  export const skillsData = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "TypeScript",
-    "React",
-    "Next.js",
-    "Tailwind",
-    "Framer Motion",
-    "Git"
-  ] as const;
+export const experiencesData = [
+  {
+    title: "Graduated University",
+    location: "Location, PL",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2015"
+  },
+  {
+    title: "Graduated University",
+    location: "Location, PL",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2022"
+  },
+  {
+    title: "Company",
+    location: "Location, PL",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet tellus cras adipiscing enim eu.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2022"
+  },
+] as const;
