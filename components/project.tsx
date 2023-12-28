8199bb8db835c3f89e730de4d4333a5f0080cb9e
Repcak Mer <58186545+Kacperem@users.noreply.github.com>
2023-12-28 20:56:35 +0100

    Update webflix added

--- components/project.tsx
+++ components/project.tsx
@@ -12,6 +12,7 @@ export default function Project({
   description,
   tags,
   imageUrl,
+  demoUrl,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
@@ -22,6 +23,11 @@ export default function Project({
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
+  const handleDemoUrl = () => {
+    const url = demoUrl;
+    window.location.href = url;
+  };
+
   return (
     <motion.div
       ref={ref}
@@ -31,10 +37,15 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className=" bg-gray-200 max-w-[42rem] borderBlack rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]  hover:bg-gray-300 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+      <section
+        onClick={handleDemoUrl}
+        className="cursor-pointer bg-gray-200 max-w-[42rem] borderBlack rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]  hover:bg-gray-300 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20"
+      >
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/80">{description}</p>
+          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/80">
+            {description}
+          </p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { LuGraduationCap } from "react-icons/lu";
 import { CgWorkAlt } from "react-icons/cg";
 import memory from "@/public/memory.png";
+import webflix from "@/public/webflix.png";
 
 export const links = [
   {
@@ -32,20 +33,26 @@ export const links = [
 
 export const projectsData = [
   {
+    title: "Webflix",
+    description:
+      "It is a clone of the popular Netflix site, which features: authentication including OAuth(GitHub, Google) add to favorites and video-player.",
+    tags: ["React", "Next.js", "Tailwind", "MongoDb", "Prisma", "SWR", "Zustand"],
+    imageUrl: webflix,
+    demoUrl: "https://webflix-one.vercel.app/",
+  },
+  {
     title: "Memory Palace [during coding]",
     description:
       "The application is designed to enable the development of the ability to quickly memorize a sequence of numbers.",
-    tags: ["React", "Material UI", "Tailwind", "Formik", "Framer Motion", ".NET Core"],
-    imageUrl:
-      memory,
-  },
-  {
-    title: "Fit App [will be a relized]",
-    description:
-      "The app is designed to make your fitness adventure easier in terms of exercise and kcal counting",
-    tags: ["React", "Tailwind", "Redux"],
-    imageUrl:
-      "https://images.unsplash.com/photo-1595675024853-0f3ec9098ac7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFwcCUyMG1lbW9yeSUyMHBhbGFjZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    tags: [
+      "React",
+      "Tailwind",
+      "Formik",
+      "Framer Motion",
+      ".NET Core",
+    ],
+    imageUrl: memory,
+    demoUrl: "",
   },
 ] as const;
 
@@ -67,7 +74,8 @@ export const experiencesData = [
     title: "GSK",
     location: "Poznan, PL",
     description_1: "IT Automation",
-    description_2: "Microsoft SQL Server Management Studio, Microsoft Azure DevOps, Symantec Workflow, Windows Server, ServiceNow, Powershell, Scrum",
+    description_2:
+      "Microsoft SQL Server Management Studio, Microsoft Azure DevOps, Symantec Workflow, Windows Server, ServiceNow, Powershell, Scrum",
     icon: React.createElement(CgWorkAlt),
     date: "01.10.2022 - currently",
   },
@@ -75,7 +83,8 @@ export const experiencesData = [
     title: "University of Science and Technology",
     location: "Wroclaw, PL",
     description_1: "Field of study: Computing and Telecommunication",
-    description_2: "Specialization: Design of Information Systems Master of Engineering",
+    description_2:
+      "Specialization: Design of Information Systems Master of Engineering",
     icon: React.createElement(LuGraduationCap),
     date: "2022",
   },
@@ -83,7 +92,8 @@ export const experiencesData = [
     title: "State University of Applied Sciences",
     location: "Krosno, PL",
     description_1: "Field of study: Computing Information",
-    description_2: "Specialization: Internet and database technologies Bachelor of Engineering",
+    description_2:
+      "Specialization: Internet and database technologies Bachelor of Engineering",
     icon: React.createElement(LuGraduationCap),
     date: "2019",
   },
